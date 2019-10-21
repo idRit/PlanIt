cf083a3663cb6681aaa9e2a23ff7441077424272
Ritwik Mukherjee <mritwik369@gmail.com>
2019-10-21 10:06:16 +0530

    njgbf

--- scripts/main.js
+++ scripts/main.js
@@ -63,7 +63,7 @@ function reqFS() {
 async function mail() {
     document.querySelector('#placename').style['text-align'] = "none";
     let body = document.querySelector('.innerpane');
-    body = `<!DOCTYPE html><div>${body.innerHTML}<a href="https://wikitravel.org/en/${countryy}">Visit Wiki for More Information!</a></div>`;
+    body = `<!DOCTYPE html><div>${body.innerHTML}<a href="https://wikitravel.org/en/${countryy}">Visit Wiki for More Information!</a><br><a href="https://planitearth.netlify.com/contact.html">Contact us!</a></div>`;
     document.querySelector('#placename').style['text-align'] = "center";
     console.log(body);
     const url = 'https://pscr.herokuapp.com/api/sendMail';
